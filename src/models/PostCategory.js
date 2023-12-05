3c2543cc894c68c42792147451f5faa787ad9d3f
devgabrielav <gabrivicentini@icloud.com>
2023-12-05 15:18:11 -0300

    Req 11 - Crie o modelo PostCategory em src/models/PostCategory.js com as propriedades e associações corretas, complete!

---
+++ src/models/PostCategory.js
@@ -0,0 +1,47 @@
+const PostCategoryModel = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define('PostCategory', {
+    postId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'blog_posts',
+        key: 'id',
+      },
+      onDelete: 'CASCADE',
+      primaryKey: true,
+    },
+
+    categoryId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'categories',
+        key: 'id',
+      },
+      onDelete: 'CASCADE',
+      primaryKey: true,
+    },
+  },
+  {
+    tableName: 'posts_categories',
+    timestamps: false,
+    underscored: true,
+  });
+
+  PostCategory.associate = (models) => {
+    models.BlogPost.belongsToMany(models.Category,{
+      as: 'blogPosts',
+      through: PostCategory,
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+    });
+    models.Category.belongsToMany(models.BlogPost,{
+      as: 'categories',
+      through: PostCategory,
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+    });
+  };
+
+  return PostCategory;
+};
+
+module.exports = PostCategoryModel;
